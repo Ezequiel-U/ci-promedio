533a4a2812b26a1ddc30357d1d5e130cab6b229d
Ezequiel Urieta <162086482+Ezequiel-U@users.noreply.github.com>
2026-03-03 23:15:01 -0700

    configuraccion de proyecto correcto para ejecutar workflow
    Add system path configuration for module import

--- test/test_promedio.py
+++ test/test_promedio.py
@@ -1,4 +1,10 @@
 import pytest
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from promedio import promedio
 from promedio import promedio
 
 # Caso correcto
